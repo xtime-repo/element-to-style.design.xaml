23075e8ef6b1a2b3ab545cfc5afa52c484536919
xtime-repo <50087835+xtime-repo@users.noreply.github.com>
2019-05-01 22:17:44 +0430

    starter pack

---
+++ script/script.js
@@ -0,0 +1,48 @@
+"use strict";
+var eSource = document.querySelector("#txtSrc");
+var eTarget = document.querySelector("#txtResponse");
+function convert() {
+    var vl = eSource.value.replace(/\>/g, ">\n");
+    var arr = vl.split("\n");
+    arr
+        .forEach(line => {
+        if (line.trim().length == 0)
+            return;
+        if (line.trim().startsWith("</")) {
+            eTarget.value += `</Style>`;
+            eTarget.value += "\n";
+            return;
+        }
+        var examinator = line.trim().split(" ");
+        examinator
+            .forEach(part => {
+            if (part.startsWith("<")) {
+                eTarget.value += `<Style TargetType="${part.substring(1)}">`;
+                eTarget.value += "\n";
+            }
+            if (part.includes("=")) {
+                eTarget.value += `     <Setter Property="${part.split("=")[0]}" Value="${part.split("=")[1].replace(/\"/g, "")}" /> `;
+                eTarget.value += "\n";
+            }
+            if (part.endsWith("/>")) {
+                eTarget.value += `</Style>`;
+                eTarget.value += "\n";
+            }
+        });
+    });
+}
+function dicUsage() {
+    eTarget.value += `    <UserControl.Resources>
+        <ResourceDictionary>
+            <ResourceDictionary.MergedDictionaries>
+                <ResourceDictionary Source="/Dashboard;component/theme/theme-header.xaml"/>
+            </ResourceDictionary.MergedDictionaries>
+        </ResourceDictionary>
+    </UserControl.Resources>`;
+    eTarget.value += "\n";
+}
+`
+<Style TargetType="Border">
+        <Setter Property="Background" Value="AliceBlue" />
+    </Style>
+    `;
